4b18a47120523457b719bcc5eba6d98627a4050e
Hyyan Abo Fakher <hyyanaf@gmail.com>
2025-05-05 08:38:37 +0200

    Add service worker registration and installation prompt functionality

--- src/main/java/com/webforj/howdy/Application.java
+++ src/main/java/com/webforj/howdy/Application.java
@@ -2,10 +2,12 @@ package com.webforj.howdy;
 
 import com.webforj.App;
 import com.webforj.annotation.AppProfile;
+import com.webforj.annotation.JavaScript;
 import com.webforj.annotation.Routify;
 import com.webforj.annotation.StyleSheet;
 
 @Routify(packages = "com.webforj.howdy.views")
 @StyleSheet("ws://app.css")
+// @JavaScript("ws://app.js")
 @AppProfile(name = "Howdy", shortName = "Howdy")
 public class Application extends App {}
